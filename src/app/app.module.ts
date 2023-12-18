9c25b05791dc89563df4d079decda8ed334aefa6
ZuraJeladze2 <zurajeladze2@gmail.com>
2023-12-18 23:05:10 +0400

    file upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
